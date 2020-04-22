93416ba151bb524a6a0ced2bfa555f5b1a703bc7
Felipe Pina <fop.net@gmail.com>
2020-04-21 21:34:14 -0300

    refatoração usando stream

--- src/main/java/greedyAlgorithms/AwsTrainning/Mdc.java
+++ src/main/java/greedyAlgorithms/AwsTrainning/Mdc.java
@@ -1,5 +1,7 @@
 package greedyAlgorithms.AwsTrainning;
 
+import java.util.Arrays;
+
 // SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
 // DEFINE ANY CLASS AND METHOD NEEDED
 // CLASS BEGINS, THIS CLASS IS REQUIRED
@@ -8,39 +10,36 @@ public class Mdc {
     public int generalizedGCD(int num, int[] arr) {
         // int prod = Arrays.asList(arr).stream().reduce(1, (a, b)-> a * b);
 
-        int prod = getProd(arr);
-        int greatest = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            int mdc = mdc(prod, arr[i]);
-            if (mdc < greatest) {
-                greatest = mdc;
-            }
-        }
+        // int prod = getProd(arr);
+        // int greatest = Integer.MAX_VALUE;
+        // for (int i = 0; i < arr.length; i++) {
+        // int mdc = mdc(prod, arr[i]);
+        // if (mdc < greatest) {
+        // greatest = mdc;
+        // }
+        // }
 
-        return greatest;
+        // return greatest;
+        int prod = getProd(arr);
+        // return Arrays.stream(arr).reduce(prod, (a, b) -> mdc(a, b));
+        return Arrays.stream(arr).map(i -> mdc(prod,i)).min().orElse(0);
 
     }
 
-        public int mdc(int dividendo, int divisor) {
+    public int mdc(int dividendo, int divisor) {
+        return (0 == divisor) ? dividendo : mdc(divisor, dividendo % divisor);
+        // if ((dividendo % divisor == 0)) {
+        // return divisor;
+        // } else {
+        // return mdc(divisor, (dividendo % divisor));
+        // }
+    }
 
-            if ((dividendo % divisor == 0)) {
-                return divisor;
-            } else {
-                return mdc(divisor, (dividendo % divisor));
-            }
-        }
-
-        int getProd(int[] arr) {
-            int prod = 1;
-            for (int i = 0; i < arr.length; i++) {
-                prod *= arr[0];
-            }
-            return prod;
-        }
-    // METHOD SIGNATURE ENDS
+    int getProd(int[] arr) {
+        return Arrays.stream(arr).reduce(1, (a, b) -> (a * b));
+    }
 
     public static void main(String[] args) {
-        Mdc s = new Mdc();
         Mdc mdc = new Mdc();
         int[] ints = { 2, 3, 4, 5, 6 };
         System.out.println(mdc.generalizedGCD(ints.length, ints));
