2a52b672cdbef5916993605c586e0d8b977a78e2
Felipe Pina <fop.net@gmail.com>
2020-04-26 16:54:35 -0300

    Conserto do workspace ao debugar pelo extensão do Java.  O problema era o nome do pacote com espaço e depois tive que fazer um clean no workspace.

--- src/test/java/greedyAlgorithms/BfsGraph/BfsTest.java
+++ src/test/java/greedyAlgorithms/BfsGraph/BfsTest.java
@@ -14,12 +14,12 @@ public class BfsTest {
     public void testBfs() {
         final GraphArray g = new GraphArray(4);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
         g.addEdge(1, 2);
-        g.addEdge(2, 0);
+        g.addEdge(1, 3);
         g.addEdge(2, 3);
-        g.addEdge(3, 3);
+        g.addEdge(3, 1);
+        g.addEdge(3, 4);
+        g.addEdge(4, 4);
 
         System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
 
